73c9076726ded1f21c6e79c09c49b21571ca953e
cbedu2 <cbedu2@uic.edu>
2019-03-17 15:06:17 -0500

    adding logging

--- downloadData.r
+++ downloadData.r
@@ -1,35 +1,66 @@
 # I used this supplemental file to download the CSVs.
 # It is not used by the Shiny application.
+
+
 library(feather)
 
+
+initLog <- function(){
+  cat("type, code, message",file="downloadData.log",sep="\n")
+}
+
+logStatus <- function(type, code, message){
+
+  cat( paste(type,code,message, sep = ", "),file="downloadData.log",sep="\n", append=TRUE)
+}
+
 downloadByYearAndName <- function(years, dataName){
+
   for(y in years) {
-    filename = paste(dataName, toString(y), ".zip", sep="")
-    dest =  paste("archives/",filename,sep="")
+    logStatus("INFO",y, "downloading years set")
+    filename <- paste(dataName, toString(y), ".zip", sep="")
+    dest <-  paste("archives/",filename,sep="")
+
     if(!file.exists(dest)){
+      logStatus("INFO",filename, "downloading file")
       url = paste("https://aqs.epa.gov/aqsweb/airdata/", filename, sep="")
       download.file(url, dest, "libcurl")
+    }else{
+      logStatus("INFO",filename, "file exists")
+    }
+
+    extractPath <- paste("./data/",filename,sep="")
+    if(!file.exists(extractPath)){
+      logStatus("INFO",filename, "unziping file")
       unzip(dest, exdir = "./data")
+    }else{
+      logStatus("INFO",extractPath, "file exists")
     }
   }
 }
 
 downloadAllData <- function(){
   dataNames <- c("hourly_44201_","hourly_42401_","hourly_42101_","hourly_42602_","hourly_88101_","hourly_81102_","hourly_WIND_","hourly_TEMP_")
-  years <-seq(from = 1990, to = 2018)
+  years <-seq(from = 1990, to = 1991)
   for(dataName in dataNames ){
+    logStatus("INFO",dataName, "downloading file set")
     downloadByYearAndName(years, dataName)
     fileNames <- list.files(path ="./data",pattern= paste(dataName,"*",sep=""), full.names = TRUE)
-    print(fileNames)
+    logStatus("INFO",dataName, "binding file set")
     dataByYear <- lapply(fileNames, read.csv)
     allData <- do.call(rbind, dataByYear)
+    logStatus("INFO", dataName, "writing to feather")
     write_feather(allData, paste("feather/", dataName,".feather", sep=""))
   }
 }
+
+
+
 dir.create("data")
 dir.create("archives")
 dir.create("feather")
 
+initLog();
 downloadAllData()
 
 download.file("https://aqs.epa.gov/aqsweb/airdata/aqs_sites.zip", "archives/aqs_sites.zip")
